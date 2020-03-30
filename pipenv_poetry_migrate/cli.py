78a0959a4b1386a9064ef04ff7cbd0e3273d2a14
Yoshiyuki HINO <yhinoz@gmail.com>
2020-03-30 23:57:18 +0900

    fix

--- pipenv_poetry_migrate/cli.py
+++ pipenv_poetry_migrate/cli.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from pipenv_poetry_migrate.migrate import migrate
+from pipenv_poetry_migrate.migrate import PipenvPoetryMigration
 
 
 def main():
@@ -10,11 +10,11 @@ def main():
     parser.add_argument('-n', '--dry-run', help='dry-run', action='store_true')
     args = parser.parse_args()
 
-    migrate(
+    PipenvPoetryMigration(
         args.pipfile,
         args.pyproject_toml,
         dry_run=args.dry_run,
-    )
+    ).migrate()
 
 
 if __name__ == '__main__':
--- pipenv_poetry_migrate/migrate.py
+++ pipenv_poetry_migrate/migrate.py
@@ -1,69 +1,70 @@
 from tomlkit import loads, dumps, table, inline_table, aot
 
 
-def load(filename):
+def load_toml(filename):
     with open(filename, 'r') as f:
         return loads(f.read())
 
 
-def save(filename, toml, *, dry_run=False):
-    if dry_run:
-        print(dumps(toml))
-        return
+class PipenvPoetryMigration(object):
+    def __init__(self, pipfile, pyproject_toml, *, dry_run=False):
+        self._pipenv = load_toml(pipfile)
+        self._pyproject = load_toml(pyproject_toml)
+        self._pyproject_toml = pyproject_toml
+        self._dry_run = dry_run
 
-    with open(filename, 'w') as f:
-        f.write(dumps(toml))
+    def migrate(self):
+        self.migrate_source()
+        self.migrate_dependencies()
+        self.migrate_dev_dependencies()
+        self.migrate_scripts()
+        self.save()
 
+    def save(self):
+        if self._dry_run:
+            print(dumps(self._pyproject))
+        else:
+            with open(self._pyproject_toml, 'w') as f:
+                f.write(dumps(self._pyproject))
 
-def migrate(pipfile, pyproject_toml, *, dry_run=False):
-    pipenv = load(pipfile)
-    pyproject = load(pyproject_toml)
+    def migrate_source(self):
+        if 'source' not in self._pipenv:
+            return
 
-    migrate_source_repo(pipenv, pyproject)
-    migrate_dependencies(pipenv, pyproject)
-    migrate_dependencies(pipenv, pyproject, dev=True)
-    migrate_scripts(pipenv, pyproject)
+        for s in self._pipenv['source']:
+            if s['name'] == 'pypi':
+                continue
 
-    save(pyproject_toml, pyproject, dry_run=dry_run)
+            source = table()
+            source.add('name', s['name'])
+            source.add('url', s['url'])
 
+            if 'source' not in self._pyproject['tool']['poetry']:
+                self._pyproject['tool']['poetry']['source'] = aot()
+            self._pyproject['tool']['poetry']['source'].add(source)
 
-def migrate_source_repo(pipenv, pyproject):
-    if 'source' not in pipenv:
-        return
+    def migrate_dependencies(self, *, dev=False):
+        prefix = 'dev-' if dev else ''
+        pipenv_key = prefix + 'packages'
+        poetry_key = prefix + 'dependencies'
 
-    for s in pipenv['source']:
-        if s['name'] == 'pypi':
-            continue
+        for name, ver in self._pipenv[pipenv_key].items():
+            if name in self._pyproject['tool']['poetry'][poetry_key]:
+                continue
+            if isinstance(ver, dict):
+                tmp = inline_table()
+                tmp.update(ver)
+                ver = tmp
+            self._pyproject['tool']['poetry'][poetry_key].add(name, ver)
 
-        source = table()
-        source.add('name', s['name'])
-        source.add('url', s['url'])
+    def migrate_dev_dependencies(self):
+        self.migrate_dependencies(dev=True)
 
-        if 'source' not in pyproject['tool']['poetry']:
-            pyproject['tool']['poetry']['source'] = aot()
-        pyproject['tool']['poetry']['source'].add(source)
+    def migrate_scripts(self):
+        if 'scripts' not in self._pipenv:
+            return
 
-
-def migrate_dependencies(pipenv, pyproject, *, dev=False):
-    prefix = 'dev-' if dev else ''
-    pipenv_key = prefix + 'packages'
-    poetry_key = prefix + 'dependencies'
-
-    for name, ver in pipenv[pipenv_key].items():
-        if name in pyproject['tool']['poetry'][poetry_key]:
-            continue
-        if isinstance(ver, dict):
-            tmp = inline_table()
-            tmp.update(ver)
-            ver = tmp
-        pyproject['tool']['poetry'][poetry_key].add(name, ver)
-
-
-def migrate_scripts(pipenv, pyproject):
-    if 'scripts' not in pipenv:
-        return
-
-    for name, cmd in pipenv['scripts'].items():
-        if 'scripts' not in pyproject['tool']['poetry']:
-            pyproject['tool']['poetry']['scripts'] = table()
-        pyproject['tool']['poetry']['scripts'].add(name, cmd)
+        for name, cmd in self._pipenv['scripts'].items():
+            if 'scripts' not in self._pyproject['tool']['poetry']:
+                self._pyproject['tool']['poetry']['scripts'] = table()
+            self._pyproject['tool']['poetry']['scripts'].add(name, cmd)
