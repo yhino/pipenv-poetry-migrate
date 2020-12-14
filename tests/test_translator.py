f5413457a40a0f02cda055d66feee48ab7f4e90b
Yoshiyuki HINO <yhinoz@gmail.com>
2020-12-14 21:01:11 +0900

    :sparkles: #7 support the git dependencies format

---
+++ tests/test_translator.py
@@ -0,0 +1,16 @@
+from pipenv_poetry_migrate.translator import translate_properties
+
+
+def test_translate_properties():
+    original = {
+        "git": "https://github.com/yhino/pipenv_poetry_migrate.git",
+        "ref": "develop",
+        "editable": True,
+    }
+
+    translated = translate_properties(original)
+    assert translated == {
+        "git": "https://github.com/yhino/pipenv_poetry_migrate.git",
+        "rev": "develop",
+        "develop": True,
+    }
