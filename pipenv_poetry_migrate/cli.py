61b42d1bbe757389c687b281733d20229000dade
Yoshiyuki HINO <yhinoz@gmail.com>
2021-01-02 14:08:48 +0900

    :sparkles: add show version option

--- pipenv_poetry_migrate/cli.py
+++ pipenv_poetry_migrate/cli.py
@@ -1,6 +1,7 @@
 import sys
 from argparse import ArgumentParser
 
+from pipenv_poetry_migrate import __version__
 from pipenv_poetry_migrate.loader import (
     PipfileNotFoundError,
     PyprojectTomlNotFoundError,
@@ -17,6 +18,9 @@ def main():
         "-t", "--pyproject-toml", type=str, required=True, help="path to pyproject.toml"
     )
     parser.add_argument("-n", "--dry-run", help="dry-run", action="store_true")
+    parser.add_argument(
+        "-v", "--version", help="show version", action="version", version=__version__
+    )
     args = parser.parse_args()
 
     try:
